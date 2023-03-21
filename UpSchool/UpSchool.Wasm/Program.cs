1166a91620b71251d07a5500c5af6e6ab1597a87
Merve Bacak <nmerveb@gmail.com>
2023-03-21 18:12:36 +0300

    Dokuzuncu ders notlari eklendi.

--- UpSchool/UpSchool.Wasm/Program.cs
+++ UpSchool/UpSchool.Wasm/Program.cs
@@ -1,6 +1,9 @@
+using Blazored.LocalStorage;
 using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using UpSchool.Wasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,5 +14,17 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 //Toaster icin eklendi.
 builder.Services.AddBlazoredToast();
+
+//Local Storage paketi icin 
+builder.Services.AddBlazoredLocalStorage(config =>
+{
+    config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    config.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+    config.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+    config.JsonSerializerOptions.WriteIndented = false;
+});
 
 await builder.Build().RunAsync();
