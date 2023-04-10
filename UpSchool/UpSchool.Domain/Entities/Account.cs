1c75d82cccd4bed465be58c9468dff350e74a8be
Merve Bacak <nmerveb@gmail.com>
2023-04-10 14:58:12 +0300

    On dorduncu ders notlari eklendi.

---
+++ UpSchool/UpSchool.Domain/Entities/Account.cs
@@ -0,0 +1,14 @@
+﻿namespace UpSchool.Domain.Entities
+{
+    //veri tabaniyla baglantili bilgileri entity icinde tanimlariz.
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string? Url { get; set; }
+        public bool IsFavourite { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+    }
+}
