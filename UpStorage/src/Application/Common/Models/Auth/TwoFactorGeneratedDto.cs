535bd72547268b077c5334d3aaa39481327470f3
nmerveb <nmerveb@gmail.com>
2023-05-26 19:48:50 +0300

    Otuz ikinci ders notlari eklendi.

---
+++ UpStorage/src/Application/Common/Models/Auth/TwoFactorGeneratedDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Common.Models.Auth
+{
+    public class TwoFactorGeneratedDto
+    {
+        public byte[] QrCodeImage { get; set; }
+
+        public string Key { get; set; } 
+    }
+}
---
+++ UpStorage/src/WebApi/Controllers/TwoFactorAuthenticationsController.cs
@@ -0,0 +1,39 @@
+﻿using Application.Common.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TwoFactorAuthenticationsController : ControllerBase
+    {
+        private readonly ITwoFactorService _twoFactorService;
+        public TwoFactorAuthenticationsController(ITwoFactorService twoFactorService)
+        {
+            _twoFactorService = twoFactorService;
+        }
+
+        [HttpPost("Generate")]
+        public IActionResult Generate(string email)
+        {
+            var twoFactorDto = _twoFactorService.Generate(email);
+
+
+            return File(twoFactorDto.QrCodeImage,"image/png");
+        }
+        
+        [HttpPost("Validate")]
+        public IActionResult Validate(string userCode)
+        {
+            var isValid = _twoFactorService.Validate(userCode);
+
+            if (isValid)
+            {
+                return Ok("You are authenticated.");
+            }
+
+            return BadRequest("Your code is not valid. Please try again.");
+        }
+    }
+}
